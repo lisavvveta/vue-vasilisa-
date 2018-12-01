085872cbb07942975011cba6e3ab0854f160f397
lisavvveta <lisavvveta@mail.ru>
2018-12-01 20:17:25 +0700

    - работа с кухонным углом

---
+++ src/views/js/table-angle.js
@@ -0,0 +1,12 @@
+const TableAngle = {
+    massiv: [
+      {
+        id: 001 ,
+        picture:"/public/img/table-angle/ritm-1.jpg",
+        name: "Ритм" ,
+        type: "кухонный угол" ,
+        size: "Размеры: 100*700+300",
+        description:"расскладывается" ,
+        prise:" ₽",
+    }]
+  }
