37d47cc280feed1085360fda3a7ec06f03fc9489
lisavvveta <lisavvveta@mail.ru>
2018-12-03 20:37:13 +0700

    - попытка работы с вь. на chair

--- src/views/chair/chair.vue
+++ src/views/chair/chair.vue
@@ -2,16 +2,14 @@
     <main>
       <!-- здесь нужно добавить header-->
       <!-- здесь нужно добавить aside (боковое меню)-->
-      <ul id="v-for-massiv" class="product" >
-        <li class="product-item image-prod ">
-          <h6 class="product-item__title"> {{name}} </h6>
-          <img v-lazy=" '/public/img/' + picture"
-                :alt=" type + ' ' + name"
-                class="product-item__image" />
-            <span class="product-item__size"> {{size}} </span>
-            <span class="product-item__description"> {{description}} </span>
-            <span class="product-item__price"> {{price}} </span>
-            <button @click=" toCart() " class="product-item__button">В корзину</button>
+      <ul id="ChairMassiv" class="product" >
+        <li v-for="item in chair" class="product-item image-prod ">
+        <h6 class="product-item__title"> {{ item.name }} </h6>
+        <image v-lazy=" '/public/img/chair/' alt"="" class="product-item__picture"> {{ item.picture }} </image>
+        <span class="product-item__size"> {{ item.size }} </span>
+        <span class="product-item__description"> {{item.description }} </span>
+        <span class="product-item__price"> {{ item.price }} </span>
+        <button v-on:click="counter += 1" class="product-item__button">В корзину</button>
         </li>
       </ul>
       <!-- здесь нужно добавить footer-->
