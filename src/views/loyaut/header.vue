78ba9c02b878ab86889a78eb85e6ae7710424e0d
lisavvveta <lisavvveta@mail.ru>
2018-12-03 22:40:50 +0700

    - доработка стульев

---
+++ src/views/loyaut/header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="header">
+      <div class="company"><img class="logotyp" src="../img/vasilisa.png" width="700" alt="logotyp"> </div>
+      <div class="href-box">
+          <section class="basket">
+              <p class="basket"> <img id="corner" src="../img/bascet.png" width="100" onclick="toggle(hidden_content)"> Корзина (<span class="count">0</span>)</p>
+              <div class="hidden" id="hidden_content">
+                  <p>Товаров:<span id="bascet" class="count"> {{ counter }} </span> </p>
+                  <p>На сумму: <span price="0" id="price">0</span> </p>
+                  <a href="order.html"> <button> Заказать звонок</button></a>
+              </div>
+          </section>
+      </div>
+      <nav class="navigation">
+          <a class="punct_menu" href="index.html"> Главная </a>
+          <a class="punct_menu" href="order.html"> Как заказать</a>
+          <a class="punct_menu" href="servis.html">Наши услуги</a>
+          <a class="punct_menu" href="comments.html">Отзывы</a>
+          <a class="punct_menu" href="feedback.html">Контакты</a>
+      </nav>
+      <div>
+          <form>
+              <p><input class="search" type="search" name="q" placeholder="Поиск по сайту">
+                  <input class="search" type="submit" value="Найти"></p>
+          </form>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
