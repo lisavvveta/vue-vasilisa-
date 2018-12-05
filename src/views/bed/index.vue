f6205dcd068a35b0e343a7062b58cc13cecc0367
lisavvveta <lisavvveta@mail.ru>
2018-12-05 19:43:32 +0700

    - созданы страницы диванов, угловых диванов, столов и фотопечатей

--- src/views/bed/index.vue
+++ src/views/bed/index.vue
@@ -1,36 +1,100 @@
 <template lang="html">
-    <main>
-      <!-- здесь нужно добавить header-->
-      <!-- здесь нужно добавить aside (боковое меню)-->
-      <ul id="v-for-massiv" class="product" >
-        <li class="product-item image-prod ">
-          <h6 class="product-item__title"> {{name}} </h6>
-          <img v-lazy=" '/public/img/' + picture"
-                :alt=" type + ' ' + name"
-                class="product-item__image" />
-            <span class="product-item__size"> {{size}} </span>
-            <span class="product-item__description"> {{description}} </span>
-            <span class="product-item__price"> {{price}} </span>
-            <button @click=" toCart() " class="product-item__button">В корзину</button>
-        </li>
-      </ul>
-      <!-- здесь нужно добавить footer-->
-    </main>
+  <main>
+    <!-- здесь нужно добавить header-->
+    <!-- здесь нужно добавить aside (боковое меню)-->
+    <ul class="product">
+      <li v-for="item in bed" class="product-item image-prod ">
+        <h6 class="product-item__title">{{ item.name }}</h6>
+        <img :src="item.picture" alt="" class="product-item__picture"/>
+        <span class="product-item__size">{{ item.size }}</span>
+        <span class="product-item__description">{{ item.description }}</span>
+        <span class="product-item__price">{{ item.price }}</span>
+        <button v-on:click="counter += 1" class="product-item__button">В корзину</button>
+      </li>
+    </ul>
+  <!-- здесь нужно добавить footer-->
+  </main>
 </template>
-
-
 
 <script>
 export default {
-
+  data () {
+    return {
+      bed: [
+      {
+        id: 001 ,
+        picture:"/public/img/bed/bruno.jpg",
+        name: "Бруно" ,
+        type: "Кровати" ,
+        size: "Размеры: 1100х1452х2064",
+        description:" с ортопедическим основанием" ,
+        prise:" ₽",
+      },
+      {
+        id: 002 ,
+        picture:"/public/img/bed/Venera.png",
+        name: "Бруно" ,
+        type: "Кровати" ,
+        size: "Размеры: 2000x1400",
+        description:"" ,
+        prise:" ₽",
+      },
+      {
+        id: 003 ,
+        picture:"/public/img/bed/fortuna.jpg",
+        name: "Фортуна" ,
+        type: "Кровати" ,
+        size: "Размеры: 2000x1400",
+        description:"" ,
+        prise:" ₽",
+      },
+      {
+        id: 004 ,
+        picture:"/public/img/bed/sicilia.jpg",
+        name: "Сицилия" ,
+        type: "Кровати" ,
+        size: "Размеры: 1600-2000",
+        description:"" ,
+        prise:" ₽",
+      },
+      {
+        id: 005 ,
+        picture:"/public/img/bed/kapry.jpg",
+        name: "Капри" ,
+        type: "Кровати" ,
+        size: "Размеры: 1600-2000",
+        description:"" ,
+        prise:" ₽",
+      },
+      {
+        id: 006 ,
+        picture:"/public/img/bed/selena.jpg",
+        name: "Селена" ,
+        type: "Кровати" ,
+        size: "Размеры: 1600-2000",
+        description:"" ,
+        prise:" ₽",
+      },
+      {
+        id: 007 ,
+        picture:"/public/img/bed/SP-121-124.jpg",
+        name: "СП-121" ,
+        type: "Кровати" ,
+        size: "Размеры: 1600-2000 ",
+        description:"Цвет: венге-дуб беленый" ,
+        prise:" ₽",
+      },
+    ]
+    }
+  }
 }
 </script>
- 
+
 
 <style lang="scss">
- 
- /* сюда импортировать файлы для слайдера а так же loyaut и для хедера и футера */ 
+
+ /* сюда импортировать файлы для слайдера а так же loyaut и для хедера и футера */
 
 </style>
 
- 
+
---
+++ src/views/comments/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <div>
+        <fieldset>
+            <legend> Отзыв</legend>
+            <form>
+                <p>Напишите ваш отзыв о магазине.</p>
+                <textarea name="comments" cols="120" rows="30"> Введите свой комментарий</textarea>
+                <input type="submit" name="send" value="отправить">
+            </form>
+        </fieldset>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass">
+
+</style>
+
