28c4aaf3c0505f83c66d90c3925becfe610b1256
lisavvveta <lisavvveta@mail.ru>
2018-12-08 00:10:10 +0700

    - сверка цен  и описаний

---
+++ src/views/hollway/index.vue
@@ -0,0 +1,67 @@
+<template lang="html">
+  <main>
+    <!-- здесь нужно добавить header-->
+    <!-- здесь нужно добавить aside (боковое меню)-->
+    <ul class="product">
+      <li v-for="item in hollway" class="product-item image-prod ">
+        <h6 class="product-item__title">{{ item.name }}</h6>
+        <img :src="item.picture" alt="" class="product-item__picture"/>
+        <span class="product-item__size">{{ item.size }}</span>
+        <span class="product-item__description">{{ item.description }}</span>
+        <span class="product-item__price">{{ item.price }}</span>
+        <button v-on:click="counter += 1" class="product-item__button">В корзину</button>
+      </li>
+    </ul>
+  <!-- здесь нужно добавить footer-->
+  </main>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      hollway: [
+      {
+        // Здесь должен быть слайдер
+        //Сюда запихнуть все рамиры, так как это один рамир, а не несколько
+        id: 01 ,
+        picture:"/public/img/hallway/ramir.jpg",
+        name: "Пpихожая со шкафом Рамир" ,
+        type: "Прихожая" ,
+        size: "1202х2100х504",
+        description:"Шкаф может быть справа или слева" ,
+        price:" от 13 500 ₽",
+      },
+      {
+        id: 02 ,
+        picture:"/public/img/hallway/nota-4.jpg",
+        name: "Прихожая Нота 4" ,
+        type: "Прихожая" ,
+        size: "2115х1000х350",
+        description:"Цвета: белый дуб" ,
+        price:" от 8 400 ₽",
+      },
+      {
+        // здесь будет слайдер
+        id: 03 ,
+        picture:"/public/img/hallway",
+        name: "Прихожая Нота 3" ,
+        type: "Прихожая" ,
+        size: "1823x400х2114",
+        description:"Модульная. Цвета: шима, венге. Вешалка 1233х1480" ,
+        price:"от 13 200 ₽",
+      },
+    ]
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+
+ /* сюда импортировать файлы для слайдера а так же loyaut и для хедера и футера */
+
+</style>
+
+
---
+++ src/views/index/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    <!-- здесь нужно добавить header-->
+    <!-- здесь нужно добавить aside (боковое меню)-->
+    <section>
+      <div class="slide">
+        <input type="radio" name="slider2" id="slider2_1" checked="checked">
+        <label for="slider2_1"></label>
+        <div> <img class="img1" src="/public/img/slider/1.jpg"></div>
+        <label for="slider2_2"></label>
+        <input type="radio" name="slider2" id="slider2_2">
+        <label for="slider2_2"></label>
+        <div><img class="img1" src="/public/img/slider/2.jpg"></div>
+        <label for="slider2_3"></label>
+        <input type="radio" name="slider2" id="slider2_3">
+        <label for="slider2_3"></label>
+        <div> <img class="img1" src=""></div>
+        <label for="slider2_4"></label>
+      </div>
+    </section>
+    <!-- здесь нужно добавить footer-->
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
